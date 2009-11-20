554485ce8b844c765cb47646514d3c1e669dac75
Sergio Lopes <slopes@gmail.com>
2009-11-20 02:59:15 -0200

    implementacao da Matriz e um teste pra ela

--- src/imagem/Matriz.scala
+++ src/imagem/Matriz.scala
@@ -1,8 +1,17 @@
 package imagem
 
 class Matriz[T] (
-  n: Int, 
-  m: Int
+  private val n: Int, 
+  private val m: Int
 ) {
-	
+  private val array = new Array[Array[T]](n, m)
+  
+  def apply(x: Int, y: Int) = array(x)(y)
+  def update(x: Int, y: Int, v: T) = array(x)(y) = v
+  
+  def linhas = n
+  def colunas = m
+}
+object Matriz {
+  def apply[T](n:Int, m:Int) = new Matriz[T](n,m)
 }
