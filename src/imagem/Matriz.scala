2d4521e3ea8d68765d5027108f9b2b1c65802af2
Sergio Lopes <slopes@gmail.com>
2009-11-20 05:42:44 -0200

    melhoria na Matriz

--- src/imagem/Matriz.scala
+++ src/imagem/Matriz.scala
@@ -4,7 +4,12 @@ class Matriz[T] (
   private val n: Int, 
   private val m: Int
 ) {
+  
+  if (n < 0 || m < 0)
+    throw new IllegalArgumentException("Dimensoes da Matriz devem ser positivas")
+  
   private val array = new Array[Array[T]](n, m)
+  
   
   def apply(x: Int, y: Int) = array(x)(y)
   def update(x: Int, y: Int, v: T) = array(x)(y) = v
