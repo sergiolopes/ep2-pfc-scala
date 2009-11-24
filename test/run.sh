892338dd9a422a417c230a031c6eaa36d6ce3597
Sergio Lopes <slopes@gmail.com>
2009-11-24 03:19:59 -0200

    rodar os testes via console ja que o eclipse nao colabora

---
+++ test/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd ..
+export CLASSPATH=lib/junit-4.7.jar:lib/scalatest-1.0.jar:bin
+
+scala org.junit.runner.JUnitCore imagem.MatrizTest
+scala org.scalatest.tools.Runner -p . -o -s util.LeituraSuite
+#scala org.scalatest.tools.Runner -p . -o -s algoritmos.SilhuetaAlgoritmos
